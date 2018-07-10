2655096717384e6e45af16e2e4de8d8f810cecdc
Piotr Szczygieł <piotr.szczygiel@makingwaves.com>
2018-07-10 11:55:33 +0200

    initial commit

---
+++ TeamCityTracker.WebJob/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace TeamCityTracker.WebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    public class Program
+    {
+
+    }
+}
