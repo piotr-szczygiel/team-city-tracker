a2afa93038e1502b6f39009854bd29c375c66a52
Piotr Szczygieł <piotr.szczygiel@makingwaves.com>
2018-07-12 21:54:01 +0200

    changes in model

--- TeamCityTracker.WebJob/Model/Build.cs
+++ TeamCityTracker.WebJob/Model/Build.cs
@@ -1,13 +1,20 @@
-﻿namespace TeamCityTracker.WebJob.Model
+﻿using Nest;
+
+namespace TeamCityTracker.WebJob.Model
 {
+    [ElasticsearchType(Name = "build")]
     public class Build
     {
+        [Number(Name = "id")]
         public int Id { get; set; }
 
+        [Text(Name = "build_type_id")]
         public string BuildTypeId { get; set; }
 
+        [Text(Name = "number")]
         public string Number { get; set; }
 
+        [Keyword(Name = "status")]
         public string Status { get; set; }
     }
 }
