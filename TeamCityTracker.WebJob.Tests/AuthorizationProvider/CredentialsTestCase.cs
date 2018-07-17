283e9b0083430b5f91206537e4019d0f69ade0e2
Piotr Szczygieł <piotr.szczygiel@makingwaves.com>
2018-07-17 08:58:38 +0200

    added unit tests for WebJob

---
+++ TeamCityTracker.WebJob.Tests/AuthorizationProvider/CredentialsTestCase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeamCityTracker.WebJob.Tests.AuthorizationProvider
+{
+    public class CredentialsTestCase
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Encoded { get; set; }
+
+        public static IEnumerable<CredentialsTestCase> GetTestCases()
+        {
+            return new[]
+            {
+                new CredentialsTestCase()
+                {
+                    Username = "John Travolta",
+                    Password = "NzWASv7LuxM6ED2H",
+                    Encoded = "Sm9obiBUcmF2b2x0YTpOeldBU3Y3THV4TTZFRDJI"
+                },
+                new CredentialsTestCase()
+                {
+                    Username = "Cristiano.Ronaldo",
+                    Password = "nwu7LMr2k6QWYdKy",
+                    Encoded = "Q3Jpc3RpYW5vLlJvbmFsZG86bnd1N0xNcjJrNlFXWWRLeQ=="
+                }
+            };
+        }
+    }
+}
---
+++ TeamCityTracker.WebJob.Tests/HttpClientBuilder/HttpClientBuilderTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+using NSubstitute;
+using NUnit.Framework;
+using TeamCityTracker.WebJob.AuthorizationProvider;
+
+namespace TeamCityTracker.WebJob.Tests.HttpClientBuilder
+{
+    [TestFixture]
+    public class HttpClientBuilderTest
+    {
+        [Test]
+        public void ShouldGetClientWithCorrectRequestHeaders()
+        {
+            // Given
+            var authorizationProvider = Substitute.For<IAuthorizationProvider>();
+            var authenticationHeaderValue = new AuthenticationHeaderValue("Basic", "SomeKey");
+            authorizationProvider.GetAuthenticationHeader().Returns(authenticationHeaderValue);
+            var clientBuilder = new WebJob.HttpClientBuilder.HttpClientBuilder(authorizationProvider);
+
+            // When
+            var httpClient = clientBuilder.GetClient();
+
+            // Then
+            Assert.That(httpClient.DefaultRequestHeaders.Authorization, Is.EqualTo(authenticationHeaderValue));   
+        }
+    }
+}
